3cea311381e207073d213331299026de8db5ee1f
Fagner Gomes <fagner.egomes@gmail.com>
2019-07-24 10:09:10 -0300

    change dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import './global.css';
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
