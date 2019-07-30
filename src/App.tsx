e74f42d7decafea05878be70c4f3888449eee72c
Fagner Gomes <fagner.egomes@gmail.com>
2019-07-30 13:05:27 -0300

    We Add Material UI and configurations the theme.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 // import './config/ReactotronConfig';
 
@@ -14,14 +16,20 @@ import { store, persistor } from '~/store';
 
 import GlobalStyles from '~/styles/global';
 
+// Theme
+import theme from './theme';
+
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyles />
-        <Toastr />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+          <Toastr />
+        </Router>
+      </ThemeProvider>
     </PersistGate>
   </Provider>
 );
